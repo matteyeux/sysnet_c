f8a1f78b885c70227b3cef769bbf2632e89c4c76
mathieu <mathieu.hautebas@gmail.com>
2017-03-08 14:47:54 +0100

    Travis build failed, may be fixed

--- include/system.h
+++ include/system.h
@@ -2,10 +2,11 @@
 #include <sys/utsname.h>
 #include <sys/statvfs.h>
 struct utsname buf;
+
 #ifdef linux
+#include <sys/sysinfo.h>
 struct sysinfo sys_info;
 #endif
-void print_ram();
 
 int version();
 int typeOS();
@@ -15,3 +16,4 @@ void use_log();
 int disk_info(const char *partition);
 int diskstat(int argc, char *argv[]);
 int raminfo();
+void print_ram();
