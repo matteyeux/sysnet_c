3a198885c53f53e16eeb845f8209a0744223ce97
Mathieu <mathieu.hautebas@gmail.com>
2017-09-07 07:53:20 -0700

    unsigned long int does not hit under 0

--- src/common.c
+++ src/common.c
@@ -5,10 +5,7 @@
 
 double convert2gb(unsigned long int total, unsigned long int val){ //function to convert to giga bytes
 	double newval;
-	if (total < 0){
-		fprintf(stderr, "%s : [ERROR] total is value is : %lu \n", __FUNCTION__, total);
-	}
-	else if (total == 0)
+	if (total == 0)
 	{
 		newval = val/pow(2,30);
 	} else {
