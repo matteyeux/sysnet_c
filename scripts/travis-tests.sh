ac90ce328639edf8246ecdd405f16d8a86e45f50
mathieu <mathieu.hautebas@gmail.com>
2019-07-15 17:27:24 +0200

    fix travis fail

--- scripts/travis-tests.sh
+++ scripts/travis-tests.sh
@@ -7,9 +7,13 @@ make
 if [[ $1 == "drone" ]]; then
 	make install
 	ldconfig
+	apt update
+	apt install libiw-dev -y
 else
 	sudo make install
 	sudo ldconfig
+	sudo apt update
+	sudo apt install libiw-dev -y
 fi
 
 cd ..
